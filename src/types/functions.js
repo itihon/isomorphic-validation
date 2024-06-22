f114adfd9ee4b6096bca92e09c1124acd4275ad4
itihon <kalabin.alexandr@gmail.com>
2024-06-22 19:43:08 +0500

    refactor(functions.js): change code to fit unit tests

--- src/types/functions.js
+++ src/types/functions.js
@@ -1,26 +1,25 @@
 import isFunction from '../utils/is-function.js';
 
 export default function Functions(iterable = [][Symbol.iterator]()) {
-  const fns = [...iterable].filter(isFunction);
+  const obj = { ...iterable, length: 0 };
+  const length = Object.keys(obj).length - 1;
+
+  obj.length = length;
+
+  const fns = Array.from(obj).filter(isFunction);
 
   function push(...args) {
     Array.prototype.push.call(fns, ...args.filter(isFunction));
     return this;
   }
 
-  // !Object assign takes less lines of code.
-  // rewrite as in ManyToManyMap
+  function run(...args) {
+    return fns.map((fn) => fn(...args));
+  }
+
   return Object.defineProperties(fns, {
-    push: {
-      value: push,
-    },
-    run: {
-      value(...args) {
-        return fns.map((fn) => fn(...args));
-      },
-    },
-    [Symbol.toStringTag]: {
-      value: Functions.name,
-    },
+    push: { value: push },
+    run: { value: run },
+    [Symbol.toStringTag]: { value: Functions.name },
   });
 }
