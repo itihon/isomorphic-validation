ac9c8638f6b627640c123db1f35868a0e3850829
itihon <kalabin.alexandr@gmail.com>
2024-07-03 17:43:10 +0500

    fix(memoize): add memoizing function's awerness of memoized function's default parameters
    when memoized function is called with missing parameters which have a default value the same result
    must be returned as with the same values explicitly passed

--- src/types/validated-item.js
+++ src/types/validated-item.js
@@ -1,40 +1,39 @@
 import memoize from '../utils/memoize.js';
 import Functions from './functions.js';
 
-const ValidatedItem = memoize(function ValidatedItem(
-  obj = {},
-  propName = '',
-  initVal = '',
-) {
-  const onRestoredCBs = Functions();
-  let savedValue = initVal;
+const ValidatedItem = memoize(
+  function ValidatedItem(obj = {}, propName = '', initVal = '') {
+    const onRestoredCBs = Functions();
+    let savedValue = initVal;
 
-  return {
-    /*
+    return {
+      /*
                 consider adding sanitizing functionality
                 use case: Number.toFixed(), String().trim(),
                 escape characters etc.
                 UPD: this kind of feature may be implemented
                 by adding the started event handler
             */
-    getObj: () => obj,
-    getValue: () => obj[propName],
-    saveValue: () => {
-      savedValue = obj[propName];
-    },
-    restoreValue: (cbArgs) => {
-      if (obj[propName] !== initVal) {
-        obj[propName] = savedValue;
-      } else {
-        // obj[propName] = lastValid = initVal;
-        savedValue = initVal;
-      }
-      onRestoredCBs.run(cbArgs);
-    },
-    onRestored: onRestoredCBs.push,
-    [Symbol.toStringTag]: ValidatedItem.name,
-  };
-});
+      getObj: () => obj,
+      getValue: () => obj[propName],
+      saveValue: () => {
+        savedValue = obj[propName];
+      },
+      restoreValue: (cbArgs) => {
+        if (obj[propName] !== initVal) {
+          obj[propName] = savedValue;
+        } else {
+          // obj[propName] = lastValid = initVal;
+          savedValue = initVal;
+        }
+        onRestoredCBs.run(cbArgs);
+      },
+      onRestored: onRestoredCBs.push,
+      [Symbol.toStringTag]: ValidatedItem.name,
+    };
+  },
+  [{}, '', ''],
+);
 
 ValidatedItem.keepValid = (
   items = [],
