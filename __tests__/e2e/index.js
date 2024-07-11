3d78987897ca63bb71da21778d3cb70f1efbce85
itihon <kalabin.alexandr@gmail.com>
2024-07-11 18:53:25 +0500

    test(e2e): add 'cancel a debounced predicate with the previous next=true' test

--- __tests__/e2e/index.js
+++ __tests__/e2e/index.js
@@ -1,7 +1,31 @@
-import { expect, jest, it, describe } from '@jest/globals';
+import { expect, jest, it, describe, beforeEach } from '@jest/globals';
 import { Validation } from '../../src/index.js';
+import {
+  isEmail,
+  isEmailNotBusy,
+  isLongerThan,
+  isNotOneTimeEmail,
+} from '../predicates.js';
+import wait from '../../src/utils/wait.js';
+
+const syncPredicate = jest.fn((value) => value === 42);
+const asyncPredicate = jest.fn(
+  (value) =>
+    new Promise((res) => {
+      setTimeout(res, 1000, value === 42);
+    }),
+);
+
+const validatedObj = {
+  value: 42,
+};
 
 describe('e2e', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    validatedObj.value = 42;
+  });
+
   it.todo(
     'valid, invalid, changed etc. accept only functions and ignore anything else',
   );
@@ -16,18 +40,6 @@ describe('e2e', () => {
   );
 
   it('is imposible to start an async predicate unless the previously launched is not finished', (done) => {
-    const syncPredicate = jest.fn((value) => value === 42);
-    const asyncPredicate = jest.fn(
-      (value) =>
-        new Promise((res) => {
-          setTimeout(res, 1000, value === 42);
-        }),
-    );
-
-    const validatedObj = {
-      value: 42,
-    };
-
     const testAsyncValidation = Validation(validatedObj);
     testAsyncValidation.constraint(syncPredicate).constraint(asyncPredicate);
 
@@ -50,10 +62,6 @@ describe('e2e', () => {
     const validCB = jest.fn();
     const invalidCB = jest.fn();
     const changedCB = jest.fn();
-
-    const validatedObj = {
-      value: 42,
-    };
 
     const initVal = '';
 
@@ -111,5 +119,167 @@ describe('e2e', () => {
     expect(validCB).toHaveBeenCalledTimes(5);
     expect(invalidCB).toHaveBeenCalledTimes(2);
     expect(changedCB).toHaveBeenCalledTimes(2);
+  });
+
+  describe('debounce', () => {
+    it('should delay running a predicate for a specified amount of time', (done) => {
+      const validation = Validation(validatedObj);
+      const debounce = 500;
+
+      validation.constraint(syncPredicate, { debounce });
+
+      const interval = setInterval(validation.validate, 200);
+
+      setTimeout(() => {
+        clearInterval(interval);
+
+        setTimeout(() => {
+          done();
+          expect(syncPredicate).toHaveBeenCalledTimes(1);
+        }, debounce);
+      }, 1200);
+    }, 3000);
+
+    it('should cancel a debounced predicate when the previous with next=false are invalid', async () => {
+      const email = { value: '' };
+      const validation = Validation(email);
+      const debounce = 300;
+      const testValues = [
+        'a@a.a',
+        'aa@a.a',
+        'a@a.',
+        'b@b.b',
+        'b@bacaki.com',
+        'q@q.q',
+      ];
+
+      validation
+        .constraint(isEmail, { next: false }) // true true false true  true  true
+        .constraint(isNotOneTimeEmail, { next: false }) // true true       true  false true
+        .constraint(isEmailNotBusy, { debounce, next: false }) // true deb.       deb.        false
+        .constraint(isLongerThan(4)); // true
+
+      // 1
+      [email.value] = testValues;
+      expect((await validation.validate()).isValid).toBe(true);
+      expect(validation.isValid).toBe(true);
+      expect(isEmail.mock.calls).toStrictEqual([['a@a.a']]);
+      expect(isNotOneTimeEmail.mock.calls).toStrictEqual([['a@a.a']]);
+      expect(isEmailNotBusy.mock.calls).toStrictEqual([['a@a.a']]);
+      expect(isLongerThan(4).mock.calls).toStrictEqual([['a@a.a']]);
+      expect(isEmail).toHaveNthReturnedWith(1, true);
+      expect(isNotOneTimeEmail).toHaveNthReturnedWith(1, true);
+      expect(isEmailNotBusy).toHaveNthReturnedWith(1, expect.any(Promise));
+      expect(isLongerThan(4)).toHaveNthReturnedWith(1, true);
+
+      // 2
+      [, email.value] = testValues;
+      validation.validate();
+      await wait(100);
+      expect(validation.isValid).toBe(true);
+      expect(isEmail.mock.calls).toStrictEqual([['a@a.a'], ['aa@a.a']]);
+      expect(isNotOneTimeEmail.mock.calls).toStrictEqual([
+        ['a@a.a'],
+        ['aa@a.a'],
+      ]);
+      expect(isEmailNotBusy.mock.calls).toStrictEqual([['a@a.a']]);
+      expect(isLongerThan(4).mock.calls).toStrictEqual([['a@a.a']]);
+      expect(isEmail).toHaveNthReturnedWith(2, true);
+      expect(isNotOneTimeEmail).toHaveNthReturnedWith(2, true);
+      expect(isEmailNotBusy).toHaveBeenCalledTimes(1); // deferred, not called yet
+      expect(isLongerThan(4)).toHaveBeenCalledTimes(1);
+
+      // 3
+      [, , email.value] = testValues;
+      expect((await validation.validate()).isValid).toBe(false);
+      expect(isEmail.mock.calls).toStrictEqual([
+        ['a@a.a'],
+        ['aa@a.a'],
+        ['a@a.'],
+      ]);
+      expect(isNotOneTimeEmail.mock.calls).toStrictEqual([
+        ['a@a.a'],
+        ['aa@a.a'],
+      ]);
+      expect(isEmailNotBusy.mock.calls).toStrictEqual([['a@a.a']]);
+      expect(isLongerThan(4).mock.calls).toStrictEqual([['a@a.a']]);
+      expect(isEmail).toHaveNthReturnedWith(3, false);
+      expect(isNotOneTimeEmail).toHaveBeenCalledTimes(2);
+      expect(isEmailNotBusy).toHaveBeenCalledTimes(1); // the deferred call is canceled, since the first predicate is invalid
+      expect(isLongerThan(4)).toHaveBeenCalledTimes(1);
+
+      // 4
+      [, , , email.value] = testValues;
+      validation.validate();
+      await wait(100);
+      expect(isEmail.mock.calls).toStrictEqual([
+        ['a@a.a'],
+        ['aa@a.a'],
+        ['a@a.'],
+        ['b@b.b'],
+      ]);
+      expect(isNotOneTimeEmail.mock.calls).toStrictEqual([
+        ['a@a.a'],
+        ['aa@a.a'],
+        ['b@b.b'],
+      ]);
+      expect(isEmailNotBusy.mock.calls).toStrictEqual([['a@a.a']]);
+      expect(isLongerThan(4).mock.calls).toStrictEqual([['a@a.a']]);
+      expect(isEmail).toHaveNthReturnedWith(4, true);
+      expect(isNotOneTimeEmail).toHaveBeenCalledTimes(3);
+      expect(isNotOneTimeEmail).toHaveNthReturnedWith(3, true);
+      expect(isEmailNotBusy).toHaveBeenCalledTimes(1); // again deferred, not called yet
+      expect(isLongerThan(4)).toHaveBeenCalledTimes(1);
+
+      // 5
+      [, , , , email.value] = testValues;
+      expect((await validation.validate()).isValid).toBe(false);
+      expect(isEmail.mock.calls).toStrictEqual([
+        ['a@a.a'],
+        ['aa@a.a'],
+        ['a@a.'],
+        ['b@b.b'],
+        ['b@bacaki.com'],
+      ]);
+      expect(isNotOneTimeEmail.mock.calls).toStrictEqual([
+        ['a@a.a'],
+        ['aa@a.a'],
+        ['b@b.b'],
+        ['b@bacaki.com'],
+      ]);
+      expect(isEmailNotBusy.mock.calls).toStrictEqual([['a@a.a']]);
+      expect(isLongerThan(4).mock.calls).toStrictEqual([['a@a.a']]);
+      expect(isEmail).toHaveNthReturnedWith(5, true);
+      expect(isNotOneTimeEmail).toHaveBeenCalledTimes(4);
+      expect(isNotOneTimeEmail).toHaveNthReturnedWith(4, false);
+      expect(isEmailNotBusy).toHaveBeenCalledTimes(1); // again canceled
+      expect(isLongerThan(4)).toHaveBeenCalledTimes(1);
+
+      // 6
+      [, , , , , email.value] = testValues;
+      expect((await validation.validate()).isValid).toBe(false);
+      expect(isEmail.mock.calls).toStrictEqual([
+        ['a@a.a'],
+        ['aa@a.a'],
+        ['a@a.'],
+        ['b@b.b'],
+        ['b@bacaki.com'],
+        ['q@q.q'],
+      ]);
+      expect(isNotOneTimeEmail.mock.calls).toStrictEqual([
+        ['a@a.a'],
+        ['aa@a.a'],
+        ['b@b.b'],
+        ['b@bacaki.com'],
+        ['q@q.q'],
+      ]);
+      expect(isEmailNotBusy.mock.calls).toStrictEqual([['a@a.a'], ['q@q.q']]);
+      expect(isLongerThan(4).mock.calls).toStrictEqual([['a@a.a']]);
+      expect(isEmail).toHaveNthReturnedWith(6, true);
+      expect(isNotOneTimeEmail).toHaveBeenCalledTimes(5);
+      expect(isNotOneTimeEmail).toHaveNthReturnedWith(5, true);
+      expect(isEmailNotBusy).toHaveBeenCalledTimes(2); // false
+      expect(isLongerThan(4)).toHaveBeenCalledTimes(1);
+    });
   });
 });
