1df47d5026fa12607d39d15b9ddd7baf48a8d597
itihon <kalabin.alexandr@gmail.com>
2024-08-07 11:02:57 +0500

    refactor(add-observable-predicate.js): remove unnecessary if condition

--- src/helpers/add-observable-predicate.js
+++ src/helpers/add-observable-predicate.js
@@ -1,4 +1,4 @@
-import { SINGLE, GROUPED, GLUED } from '../constants.js';
+import { SINGLE, GLUED } from '../constants.js';
 import Predicate from '../types/predicate.js';
 import ManyToManyMap from '../types/many-to-many-map.js';
 import ObservablePredicate from '../types/observable-predicate.js';
@@ -15,21 +15,6 @@ export default function addObservablePredicate(
     anyData,
   } = {},
 ) {
-  if (TYPE === SINGLE || TYPE === GROUPED) {
-    return function forSingleOrMulti(predicateGroup, key) {
-      predicateGroup.add(
-        ObservablePredicate(
-          Predicate(predicate),
-          [...items.get(key)],
-          keepValid,
-          optional,
-          anyData,
-        ),
-        { next, debounce },
-      );
-    };
-  }
-
   if (TYPE === GLUED) {
     const op = ObservablePredicate(
       predicate,
@@ -38,11 +23,22 @@ export default function addObservablePredicate(
       optional,
       anyData,
     );
+
     return function forGlued(predicateGroup /* key */) {
       predicateGroup.add(op, { next, debounce });
     };
   }
 
-  // eslint consistent-return
-  return function nop() {};
+  return function forSingleOrGrouped(predicateGroup, key) {
+    predicateGroup.add(
+      ObservablePredicate(
+        Predicate(predicate),
+        [...items.get(key)],
+        keepValid,
+        optional,
+        anyData,
+      ),
+      { next, debounce },
+    );
+  };
 }
