be27501c8b5f05d777189516e7f489e9fdd4fa6e
itihon <kalabin.alexandr@gmail.com>
2025-02-08 16:49:53 +0500

    chore(): Release version v0.6.2

--- src/ui/set-outline-by-validity2.js
+++ src/ui/set-outline-by-validity2.js
@@ -1,12 +1,12 @@
 import setByValidity from './set-by-validity2.js';
 
 const setOutlineEffect = (element, outlines, isValid) => {
-  element.style.outline = outlines[isValid];
+  element.style.outline = outlines[isValid].value;
 };
 
 const setOutlineByValidity = setByValidity(setOutlineEffect, {
-  true: '1px solid green',
-  false: '1px solid red',
+  true: { delay: 0, value: '1px solid green' },
+  false: { delay: 0, value: '1px solid red' },
 });
 
 export default setOutlineByValidity;
