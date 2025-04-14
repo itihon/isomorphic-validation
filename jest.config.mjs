7b1e8da83de90601792ba56b3696efb8d3d6619a
itihon <kalabin.alexandr@gmail.com>
2025-04-14 10:34:05 +0500

    chore(jest.config.mjs): add regex to match test files

--- jest.config.mjs
+++ jest.config.mjs
@@ -159,6 +159,9 @@ const config = {
   //   "**/__tests__/**/*.[jt]s?(x)",
   //   "**/?(*.)+(spec|test).[tj]s?(x)"
   // ],
+  testMatch: [
+    "**/__tests__/**/*test.[jt]s?(x)"
+  ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
